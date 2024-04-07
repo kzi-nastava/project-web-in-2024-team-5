b8827092e8994d63316eff93e0cd8f3b7baec1d3
Stefan Crepulja <stefan.crepulja@gmail.com>
2024-04-07 13:37:59 +0200

    Review class

--- web-shop/src/main/java/com/webshop/model/Review.java
+++ web-shop/src/main/java/com/webshop/model/Review.java
@@ -1,8 +1,25 @@
 package com.webshop.model;
+import jakarta.persistence.*;
 
+import javax.swing.plaf.nimbus.State;
+import java.util.Date;
 /**
  * Review
  */
+@Entity
 public class Review {
+    private Integer score;
+    private String comment;
+    private Date reviewDate;
+    private User user;
+    @Id
+    private Long id;
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
