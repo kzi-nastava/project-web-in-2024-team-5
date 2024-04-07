1bdbfe79be18bc03b645743d37c99f33104844e3
Stefan Crepulja <stefan.crepulja@gmail.com>
2024-04-07 13:37:51 +0200

    Product class

--- web-shop/src/main/java/com/webshop/model/Product.java
+++ web-shop/src/main/java/com/webshop/model/Product.java
@@ -1,7 +1,42 @@
 package com.webshop.model;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+enum typeOfSale {FIXED_PRICE, AUCTION}
+
 /**
  * Product
  */
+@Entity
 public class Product {
+    private String name;
+    private String imagePath; // mozda, mozda i ne ?
+    private String description;
+    @ManyToOne
+    private Category category;
+    private Float price;
+    private typeOfSale typeOfSale;
+    private Date saleStartDate;
+    @ManyToOne
+    private Seller seller;
+    private Boolean buyerReview;
+    private Boolean sellerReview;
+    private Boolean sold;
+    @OneToMany
+    private ArrayList<Offer> offers;
+
+    @Id
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
+
