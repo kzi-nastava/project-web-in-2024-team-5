ed22d622971f2b5ed24ab85cae077cf9122a3945
Stefan Crepulja <stefan.crepulja@gmail.com>
2024-04-12 19:45:46 +0200

    Added ReportStatusRepository class

---
+++ web-shop/src/main/java/com/webshop/repository/ReportStatusRepository.java
@@ -0,0 +1,7 @@
+package com.webshop.repository;
+import com.webshop.model.ReportStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportStatusRepository extends JpaRepository<ReportStatus, Long> {
+
+}
