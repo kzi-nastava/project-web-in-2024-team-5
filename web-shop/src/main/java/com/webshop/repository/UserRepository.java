1190627a57681a3baf74a66f82ca671f18be1d92
Stefan Crepulja <stefan.crepulja@gmail.com>
2024-04-13 14:35:08 +0200

    Added queries for User Repository

--- web-shop/src/main/java/com/webshop/repository/UserRepository.java
+++ web-shop/src/main/java/com/webshop/repository/UserRepository.java
@@ -2,5 +2,12 @@ package com.webshop.repository;
 import com.webshop.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByEmail(String email);
+    List<User> findByUsername(String username);
+    List<User> findByEmailAndLastname(String email, String lastname);
+    List<User> findDistinctPeopleByFirstnameOrLastname(String firstname, String lastname);
+    
 }
