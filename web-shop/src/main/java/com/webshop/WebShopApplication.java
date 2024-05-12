f0392cb784c2107a92e09b4691da074f9f536bc2
Stefan Crepulja <stefan.crepulja@gmail.com>
2024-05-12 19:46:38 +0200

    main fixed

--- web-shop/src/main/java/com/webshop/WebShopApplication.java
+++ web-shop/src/main/java/com/webshop/WebShopApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.webshop.model.Poster;
-import com.webshop.model.Review;
 
 /*
    @SpringBootApplication anotacija nastala je od @EnableAutoConfiguration anotacije koja
