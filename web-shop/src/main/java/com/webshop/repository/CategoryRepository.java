bef9de85f81d5b9c6efce3c64e7f26c90052e92a
Stefan Crepulja <stefan.crepulja@gmail.com>
2024-05-12 19:42:20 +0200

    Fixed misssing annotations in repositories..

--- web-shop/src/main/java/com/webshop/repository/CategoryRepository.java
+++ web-shop/src/main/java/com/webshop/repository/CategoryRepository.java
@@ -1,6 +1,8 @@
 package com.webshop.repository;
 import com.webshop.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
