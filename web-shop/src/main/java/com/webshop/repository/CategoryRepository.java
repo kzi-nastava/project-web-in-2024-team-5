939f01dc4fc6a775492d1e3691eb2dd0ed8790a1
Aleksa Mrda <aleksamrda@gmail.com>
2024-05-16 00:27:59 +0200

    Add: find by category name

--- web-shop/src/main/java/com/webshop/repository/CategoryRepository.java
+++ web-shop/src/main/java/com/webshop/repository/CategoryRepository.java
@@ -1,6 +1,9 @@
 package com.webshop.repository;
+
 import com.webshop.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Category findByCategoryName(String name);
 }
