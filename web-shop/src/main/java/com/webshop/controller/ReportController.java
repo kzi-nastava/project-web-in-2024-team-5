99f9889eb933daa634a7a31eeadfd003f6b10aba
Stefan <stefan.crepulja@gmail.com>
2024-05-20 16:16:26 +0200

    Implemented Report Controller

---
+++ web-shop/src/main/java/com/webshop/controller/ReportController.java
@@ -0,0 +1,60 @@
+package com.webshop.controller;
+
+import com.webshop.dto.ReportDto;
+import com.webshop.model.Product;
+import com.webshop.repository.UserRepository;
+import com.webshop.service.*;
+import com.webshop.session.UserSession;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/api/v1/reports")
+public class ReportController {
+
+    @Autowired
+    private ReportServiceImpl reportServiceImpl;
+
+    @PostMapping("/{id}")
+    public ResponseEntity<String> reportUser(HttpSession session, @PathVariable Long id, @RequestBody Map<String, String> reason) {
+        UserSession userSession = (UserSession) session.getAttribute("User");
+        if(userSession == null) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        Long userId = userSession.getId();
+        if(!reason.containsKey("reason")) {
+            return new ResponseEntity<>("You must provide a reason for report", HttpStatus.BAD_REQUEST);
+        }
+        boolean success = reportServiceImpl.reportUser(userId, id, String.valueOf(reason.get("reason")));
+        if(success) {
+            return new ResponseEntity<>("Report successfully submitted", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Report failed", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    @PostMapping("/resolve/{id}")
+    public ResponseEntity<String> resolveReport(HttpSession session, @PathVariable Long id, @RequestBody Map<String, String> resolution) {
+        UserSession userSession = (UserSession) session.getAttribute("User");
+        if(userSession == null) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        if(!userSession.getRole().equals("admin")) {
+            return new ResponseEntity<>("You're not an admin.", HttpStatus.UNAUTHORIZED);
+        }
+        if(!resolution.containsKey("resolution")) {
+            return new ResponseEntity<>("You must provide a resolution for report", HttpStatus.BAD_REQUEST);
+        }
+        boolean success = reportServiceImpl.resolveReport(id, resolution.get("resolution"));
+        if(success) {
+            return new ResponseEntity<>("Report successfully resolved", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Something went wrong with resolving report.", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
