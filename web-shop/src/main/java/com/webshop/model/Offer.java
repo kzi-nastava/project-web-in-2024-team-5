7b7c204b5227a06e56b7777d92a05cdb27548c3c
Stefan Crepulja <stefan.crepulja@gmail.com>
2024-05-23 01:54:47 +0200

    Fixed generating ID's when adding new offers.

--- web-shop/src/main/java/com/webshop/model/Offer.java
+++ web-shop/src/main/java/com/webshop/model/Offer.java
@@ -17,7 +17,7 @@ import jakarta.persistence.Table;
 @Table(name = "offers")
 public class Offer implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
