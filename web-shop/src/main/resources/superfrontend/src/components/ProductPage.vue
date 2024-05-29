3dc4c8b9abf5379949d2cfc5545d5ac72c420979
Stefan Crepulja <stefan.crepulja@gmail.com>
2024-05-29 17:43:19 +0200

    Created component for presenting product and it's extended details.

---
+++ web-shop/src/main/resources/superfrontend/src/components/ProductPage.vue
@@ -0,0 +1,20 @@
+<script>
+import { getImageUrl } from './utils';
+export default {
+    methods: {
+        getImageUrl
+    },
+};
+</script>
+
+<template>
+    <div>
+      <h1>Product details:</h1>
+      <h2>ID: {{ $route.params.id }}</h2>
+      <h2>Name: {{ $route.query.name }}</h2>
+      <h2>Price: {{ $route.query.price }}</h2>
+      <img :src = "getImageUrl($route.query.img)">
+
+      <!-- Fetch product details using the received props -->
+    </div>
+  </template>
