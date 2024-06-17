908c96ab1586488de076c551ba45de9365366c4d
Stefan Crepulja <stefan.crepulja@gmail.com>
2024-06-17 23:27:15 +0200

    Updated home view

--- web-shop/src/main/resources/superfrontend/src/views/Home.vue
+++ web-shop/src/main/resources/superfrontend/src/views/Home.vue
@@ -1,16 +1,15 @@
 <template>
     <Header/>
-    <div class = "ml-12"><Sidebar/></div>
+    <RouterView/>
 </template>
 
 <script>
 import HomePage from '@/components/HomePage.vue';
-import Sidebar from '@/components/Sidebar.vue';
 import Header from '@/components/Header.vue'
-import Products from '@/components/ProductsPage.vue'
 
     export default {
-    components: {Sidebar, Header, HomePage}
+    components: {Header, HomePage}
+    
 }
 </script>
 
