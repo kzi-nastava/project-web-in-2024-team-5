aa589620ccfbbb31b84c07512c57d58a70d8d7d1
Stefan Crepulja <stefan.crepulja@gmail.com>
2024-06-22 14:11:35 +0200

    Fixed routing, there was a problem while importing views.

--- web-shop/src/main/resources/superfrontend/src/App.vue
+++ web-shop/src/main/resources/superfrontend/src/App.vue
@@ -1,9 +1,8 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import Home from './views/Home.vue'
+import { RouterLink, RouterView } from "vue-router";
+import Home from "./views/Home.vue";
 </script>
 
 <template>
- <Home/>
+  <RouterView />
 </template>
-
--- web-shop/src/main/resources/superfrontend/src/router/index.js
+++ web-shop/src/main/resources/superfrontend/src/router/index.js
@@ -1,54 +1,55 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '@/views/Home.vue'
-import AllProducts from '@/views/Products.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/views/Home.vue";
+import AllProducts from "@/views/Products.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home'
+      path: "/",
+      name: "home",
+      component: () => import("../views/Home.vue"),
     },
     {
-      path: '/about',
-      name: 'about',
-      
-      component: () => import('../views/About.vue')
+      path: "/about",
+      name: "about",
+
+      component: () => import("../views/About.vue"),
     },
     {
-      path: '/logout',
-      name: 'logout',
-      component: () => import('../components/LogoutPage.vue'), 
+      path: "/logout",
+      name: "logout",
+      component: () => import("../components/LogoutPage.vue"),
     },
     {
-      path: '/register',
-      name: 'register',
-      component: () => import('../components/RegisterPage.vue')
+      path: "/register",
+      name: "register",
+      component: () => import("../components/RegisterPage.vue"),
     },
     {
-      path: '/login',
-      name: 'login',
-      component: () => import('../components/LoginPage.vue')
+      path: "/login",
+      name: "login",
+      component: () => import("../components/LoginPage.vue"),
     },
     {
-      path: '/products',
-      name: 'products',
-      component: () => import('../views/Products.vue')
+      path: "/products",
+      name: "products",
+      component: () => import("../views/Products.vue"),
     },
     {
-      path: '/products/:id',
-      name: 'product',
-      component: () => import('../components/ProductPage.vue'),
-      props: true
+      path: "/products/:id",
+      name: "product",
+      component: () => import("../views/Product.vue"),
+      props: true,
     },
     {
-      path: '/:catchAll(.*)', redirect: '/register'
-    }
-  ]
+      path: "/:catchAll(.*)",
+      redirect: "/register",
+    },
+  ],
 });
 router.beforeEach((to, from, next) => {
-  const publicPages = ['/','/products','/login', '/register'];
+  const publicPages = ["/", "/products", "/login", "/register"];
   const authRequired = !publicPages.includes(to.path);
-
 
   next();
 });
