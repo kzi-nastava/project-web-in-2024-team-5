1ce1c66bd2517aa02e0a050a87fcca733e51915a
Stefan Crepulja <stefan.crepulja@gmail.com>
2024-06-22 18:22:28 +0200

    Simple fixes..

--- web-shop/src/main/resources/superfrontend/src/views/Products.vue
+++ web-shop/src/main/resources/superfrontend/src/views/Products.vue
@@ -1,18 +1,20 @@
 <template>
-    <div class = "ml-96"><ProductsPage /></div>
+  <Header />
+  <ProductsPage />
+  <Footer />
 </template>
 
 <script>
-import HomePage from '@/components/HomePage.vue';
-import Sidebar from '@/components/Sidebar.vue';
-import Header from '@/components/Header.vue'
-import ProductsPage from '@/components/ProductsPage.vue'
+import HomePage from "@/components/HomePage.vue";
+import Sidebar from "@/components/Sidebar.vue";
+import Header from "@/components/Header.vue";
+import ProductsPage from "@/components/ProductsPage.vue";
+import Footer from "@/components/FooterComp.vue";
 
-    export default {
-    components: {Sidebar, ProductsPage}
-}
+export default {
+  components: { Header, ProductsPage, Footer },
+};
 </script>
 
-<style>
+<style></style>
 
-</style>
