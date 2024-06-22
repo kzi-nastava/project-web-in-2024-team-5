5c4f3b1fe639db36acbb271e3b1529188dd5be97
Stefan Crepulja <stefan.crepulja@gmail.com>
2024-06-22 18:21:46 +0200

    Edited routing so that it pushes URL to home page instead of login, after loging out

--- web-shop/src/main/resources/superfrontend/src/components/LogoutPage.vue
+++ web-shop/src/main/resources/superfrontend/src/components/LogoutPage.vue
@@ -1,34 +1,29 @@
 <template>
-    <div>
-        <h1>
-           Here you can log out! 
-        </h1>
-        <button @click = "logout">Logout</button>
-    </div>
+  <div>
+    <h1>Here you can log out!</h1>
+    <button @click="logout">Logout</button>
+  </div>
 </template>
 
 <script>
-    import axios from 'axios';
-    import EventBus from './event'
-    export default {
-        
-        methods:{
-        async logout() {
-            try {
-                const response = await axios.post("http://localhost:8080/api/v1/logout");
-                this.$router.push('/login');
-                EventBus.$emit('userLoggedOut');
-                console.log(response.data);
-            }
-            catch(error) {
-                console.error(error);
-            }
-
-        }
-     }
-    }
+import axios from "axios";
+export default {
+  methods: {
+    async logout() {
+      try {
+        const response = await axios.post(
+          "http://localhost:8080/api/v1/logout",
+        );
+        this.$router.push("/");
+        EventBus.$emit("userLoggedOut");
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+};
 </script>
 
-<style>
+<style></style>
 
-</style>
