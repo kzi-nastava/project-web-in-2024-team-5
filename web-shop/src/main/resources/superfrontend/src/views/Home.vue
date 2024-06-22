4c02920dbd87db3a9b16c2d25c1b470066b81f72
Stefan Crepulja <stefan.crepulja@gmail.com>
2024-06-22 13:28:27 +0200

    Updatet home view

--- web-shop/src/main/resources/superfrontend/src/views/Home.vue
+++ web-shop/src/main/resources/superfrontend/src/views/Home.vue
@@ -2,16 +2,17 @@
   <Header />
   <RouterView />
   <HomePage />
+  <Footer />
 </template>
 
 <script>
 import HomePage from "@/components/HomePage.vue";
+import Footer from "@/components/FooterComp.vue";
 import Header from "@/components/Header.vue";
 
 export default {
-  components: { Header, HomePage },
+  components: { Header, HomePage, Footer },
 };
 </script>
 
 <style></style>
-
