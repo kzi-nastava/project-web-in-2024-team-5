5639f6f16f035d92ff834f2b8b4e74cd711c28a3
Aleksa Mrda <aleksamrda@gmail.com>
2024-06-23 12:02:44 +0200

    Product card component routing support

--- web-shop/src/main/resources/superfrontend/src/components/ProductCardComp.vue
+++ web-shop/src/main/resources/superfrontend/src/components/ProductCardComp.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="border p-4 rounded-lg shadow">
-    <img :src="productImage" alt="Product Image" class="mb-4" />
-    <h3 class="text-lg font-bold mb-2">{{ product.name }}</h3>
-    <p class="text-gray-700 mb-4">{{ product.price }} rsd</p>
-    <button class="text-blue-500">more...</button>
+    <router-link
+      :to="{
+        name: 'product',
+        params: { id: product.id },
+      }"
+    >
+      <img :src="productImage" alt="Product Image" class="mb-4" />
+      <h3 class="text-lg font-bold mb-2">{{ product.name }}</h3>
+      <p class="text-gray-700 mb-4">{{ product.price }} rsd</p>
+      <p class="text-blue-500">more...</p>
+    </router-link>
   </div>
 </template>
 
