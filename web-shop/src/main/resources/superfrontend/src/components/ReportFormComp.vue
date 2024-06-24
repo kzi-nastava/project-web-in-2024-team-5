543c4631e74af010ea9b03e63a3bacfcd65f4336
Stefan Crepulja <stefan.crepulja@gmail.com>
2024-06-24 21:25:44 +0200

    Created forms for posting reviews/reports

--- web-shop/src/main/resources/superfrontend/src/components/ReportFormComp.vue
+++ web-shop/src/main/resources/superfrontend/src/components/ReportFormComp.vue
@@ -1,4 +1,42 @@
-<template></template>
+<template>
+    <div class = "container w-[1200px] mx-auto">
+    <div class = "flex flex-col font-[Open_Sans] my-4 text-2xl">
+            <h2 class = "font-semibold text-3xl">Popunite formu za prijavljivanje korisnika.</h2>
+            <div class = "bg-white rounded-2xl shadow-xl">
+            <form @submit.prevent="postreport" class = "flex flex-col p-2 space-y-4">
+    <label for = "comment">Razlog prijave</label>
+    <input v-model = "report.reason" form = "text" placeholder="Unesi razlog" id = "comment"></input>
+
+                    <input class ="justify-self-end drop-shadow-lg bg-red-600 hover:bg-red-800 text-white h-full rounded-2xl " type = "submit" value = "Postavi prijavu"></input> 
+
+            </form>
+            </div>
+    </div>
+    </div>
+</template>
 <script>
-export default {};
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            report: {},
+        }
+    },
+    computed: {
+        userId() {
+            return this.$route.params.id;
+        }
+    },
+    methods: {
+    async postreport() {
+            try {
+        const response = await axios.post(`http://localhost:8080/api/v1/reports/${this.userId}`, this.report);
+                console.log(response);
+            }
+            catch(error) {
+                
+            }
+    },
+    },
+};
 </script>
--- web-shop/src/main/resources/superfrontend/src/components/ReviewFormComp.vue
+++ web-shop/src/main/resources/superfrontend/src/components/ReviewFormComp.vue
@@ -1,4 +1,44 @@
-<template></template>
+<template>
+    <div class = "container w-[1200px] mx-auto">
+    <div class = "flex flex-col font-[Open_Sans] my-4 text-2xl">
+            <h2 class = "font-semibold text-3xl">Popunite formu za ocenjivanje korisnika.</h2>
+            <div class = "bg-white rounded-2xl shadow-xl">
+            <form @submit.prevent="postReview" class = "flex flex-col p-2 space-y-4">
+    <label for = "score">Ocena</label>
+    <input v-model = "review.score" form = "text" maxlength ="50" placeholder="Unesi ocenu za korisnika" id = "score"></input>
+    <label for = "comment">Komentar</label>
+    <input v-model = "review.comment" form = "text" placeholder="Unesi komentar" id = "comment"></input>
+
+                    <input class ="justify-self-end drop-shadow-lg bg-[#004E9D] text-white h-full rounded-2xl " type = "submit" value = "Postavi recenziju"></input> 
+
+            </form>
+            </div>
+    </div>
+    </div>
+</template>
 <script>
-export default {};
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            review: {},
+        }
+    },
+    computed: {
+        userId() {
+            return this.$route.params.id;
+        }
+    },
+    methods: {
+    async postReview() {
+            try {
+        const response = await axios.post(`http://localhost:8080/api/v1/reviews/${this.userId}`, this.review);
+                console.log(response);
+            }
+            catch(error) {
+                
+            }
+    },
+    },
+};
 </script>
