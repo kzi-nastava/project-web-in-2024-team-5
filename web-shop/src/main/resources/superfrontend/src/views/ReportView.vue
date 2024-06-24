bb077110e1da0a7786d67dfc0ac9174ce1e6ad44
Stefan Crepulja <stefan.crepulja@gmail.com>
2024-06-24 20:27:52 +0200

    Added separate views for reporting and reviewing someone

---
+++ web-shop/src/main/resources/superfrontend/src/views/ReportView.vue
@@ -0,0 +1,17 @@
+<template>
+  <Header />
+  <ReportFormComp />
+  <Footer />
+</template>
+<script>
+import Header from "@/components/Header.vue";
+import ReportFormComp from "@/components/ReportFormComp.vue";
+import Footer from "@/components/FooterComp.vue";
+export default {
+  components: {
+    Header,
+    ReportFormComp,
+    Footer,
+  },
+};
+</script>
