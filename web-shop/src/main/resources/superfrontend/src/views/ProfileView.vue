1a1c3e584569dda830edee6c974cd82c7481e366
Stefan Crepulja <stefan.crepulja@gmail.com>
2024-06-24 20:28:12 +0200

    Implemented viewing profiles of other users

---
+++ web-shop/src/main/resources/superfrontend/src/views/ProfileView.vue
@@ -0,0 +1,17 @@
+<template>
+  <Header />
+  <ProfileComp />
+  <Footer />
+</template>
+<script>
+import Header from "@/components/Header.vue";
+import ProfileComp from "@/components/ViewProfileComp.vue";
+import Footer from "@/components/FooterComp.vue";
+export default {
+  components: {
+    Header,
+    ProfileComp,
+    Footer,
+  },
+};
+</script>
